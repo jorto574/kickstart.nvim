107776289e8422de122a2794109e64a355ae1181
Ioannis Giannoukos <ioannhsyan76@gmail.com>
2025-01-24 11:43:54 +0200

    local keymap = vim.keymap.set

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,14 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+local keymap = vim.keymap.set
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+keymap('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { desc = 'Open [D]iagnostic quickfix list' })
+keymap('n', '<leader>d', vim.diagnostic.setloclist, { desc = 'Open [D]iagnostic quickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -14,22 +16,22 @@ vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { desc = 'Open [D]ia
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- keymap('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- keymap('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- keymap('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- keymap('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+keymap('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+keymap('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -46,37 +48,37 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Write/Quit buffer
-vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = '[W]rite current buffer' })
-vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = '[Q]uit current buffer' })
+keymap('n', '<leader>w', ':w<CR>', { desc = '[W]rite current buffer' })
+keymap('n', '<leader>q', ':q<CR>', { desc = '[Q]uit current buffer' })
 
 -- Resize window
-vim.keymap.set('n', '<C-Up>', '<C-w>+')
-vim.keymap.set('n', '<C-Down>', '<C-w>-')
-vim.keymap.set('n', '<C-Left>', '<C-w><')
-vim.keymap.set('n', '<C-Right>', '<C-w>>')
+keymap('n', '<C-Up>', '<C-w>+')
+keymap('n', '<C-Down>', '<C-w>-')
+keymap('n', '<C-Left>', '<C-w><')
+keymap('n', '<C-Right>', '<C-w>>')
 
 -- Moving code in visual mode
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
+keymap('v', 'J', ":m '>+1<CR>gv=gv")
+keymap('v', 'K', ":m '<-2<CR>gv=gv")
+keymap('v', '<', '<gv')
+keymap('v', '>', '>gv')
 
 -- Switching Tabs
-vim.keymap.set('n', '<TAB>', 'gt')
-vim.keymap.set('n', '<S-TAB>', 'gT')
+keymap('n', '<TAB>', 'gt')
+keymap('n', '<S-TAB>', 'gT')
 
 -- Visuals for moving around in window
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '<C-f>', '<C-f>zz')
-vim.keymap.set('n', '<C-b>', '<C-b>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+keymap('n', '<C-d>', '<C-d>zz')
+keymap('n', '<C-u>', '<C-u>zz')
+keymap('n', '<C-f>', '<C-f>zz')
+keymap('n', '<C-b>', '<C-b>zz')
+keymap('n', 'n', 'nzzzv')
+keymap('n', 'N', 'Nzzzv')
 
 -- The following keymap joins the current line with the next one and
 -- does not move the cursor from its original position. The default
 -- behavior is to join the two lines and move the cursor to the merging point.
 -- The temporary mark that is being created is deleted in the end.
-vim.keymap.set('n', 'J', 'mzJ`z<cmd>delm z<CR>')
+keymap('n', 'J', 'mzJ`z<cmd>delm z<CR>')
 
 -- vim: ts=2 sts=2 sw=2 et
